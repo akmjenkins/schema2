9ca23188bf025edd8b09c183f91df58b0512aa15
akmjenkins <akmjenkins@gmail.com>
2021-08-03 23:22:52 -0300

    Date has already been parsed, no need to parse it again

--- src/schemas/date/transforms/tersity.js
+++ src/schemas/date/transforms/tersity.js
@@ -1,9 +1,9 @@
 import { getTersity } from '../utils';
 
 const tersity =
-  ({ parser }) =>
+  () =>
   ({ value: v }) =>
   (value, { resolve }) =>
-    getTersity(parser(value), resolve(v));
+    getTersity(value, resolve(v));
 
 export default tersity;

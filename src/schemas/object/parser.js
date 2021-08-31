2aa65b5c6c086a963b9c3b03bc7a1e9cb8bb25a5
akmjenkins <akmjenkins@gmail.com>
2021-08-31 16:05:09 -0300

    Be more specific about which keys to include in objects

--- src/schemas/object/parser.js
+++ src/schemas/object/parser.js
@@ -1,13 +1,13 @@
 import { hasOwnProp, canBailOut } from '../../utils';
 
-export default ({ inner = {} }, value = {}, options, check) => {
-  return Object.entries(inner).reduce(
+export default ({ inner = {} }, value = {}, options, check) =>
+  Object.entries(inner).reduce(
     (acc, [key, schemaOrRef]) => {
       if (canBailOut(options, acc.results)) return acc;
       const { value, results } = check(schemaOrRef, acc.value[key], key);
       return {
         value:
-          value || hasOwnProp(acc.value, key)
+          value !== undefined || hasOwnProp(acc.value, key)
             ? { ...acc.value, [key]: value }
             : acc.value,
         results: [...acc.results, ...results],
@@ -15,4 +15,3 @@ export default ({ inner = {} }, value = {}, options, check) => {
     },
     { value, results: [] },
   );
-};
